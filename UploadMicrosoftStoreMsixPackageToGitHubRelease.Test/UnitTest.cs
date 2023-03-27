efc501f06e736d40f2cf4bb8958e4e0d30b79e01
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2023-03-27 22:02:42 +0800

    Update unit tests

--- UploadMicrosoftStoreMsixPackageToGitHubRelease.Test/UnitTest.cs
+++ UploadMicrosoftStoreMsixPackageToGitHubRelease.Test/UnitTest.cs
@@ -3,24 +3,29 @@ namespace UploadMicrosoftStoreMsixPackageToGitHubRelease.Test;
 public class UnitTest
 {
     [Theory]
-    [InlineData("1.2.3.4", "1.2.3.4")]
-    [InlineData("1.2.3.4", "1.2.3")]
-    [InlineData("1.2.3.4", "1.2.3.5")]
+    [InlineData("1.2.3.0", "1.2.3.0")]
+    [InlineData("1.2.3.0", "1.2.3")]
+    [InlineData("1.2.3.1", "1.2.3")]
+    [InlineData("1.2.3.0", "v1.2.3.0")]
+    [InlineData("1.2.3.0", "v1.2.3")]
+    [InlineData("1.2.3.1", "v1.2.3")]
     public void CommonHelper_MsixPackageAndGitHubReleaseVersionsAreEqual_True(string msixPackageVersion, string gitHubReleaseTagName)
     {
         Assert.True(CommonHelper.MsixPackageAndGitHubReleaseVersionsAreEqual(msixPackageVersion, gitHubReleaseTagName));
     }
 
     [Theory]
-    [InlineData("1.2.3.4", "0.1.2.3")]
-    [InlineData("1.2.3.4", "invalid-tag-name")]
+    [InlineData("1.2.3.0", "1.2.4.0")]
+    [InlineData("1.2.3.0", "1.3.3")]
+    [InlineData("1.2.3.0", "v2.0.0")]
+    [InlineData("1.2.3.0", "invalid-tag-name")]
     public void CommonHelper_MsixPackageAndGitHubReleaseVersionsAreEqual_False(string msixPackageVersion, string gitHubReleaseTagName)
     {
         Assert.False(CommonHelper.MsixPackageAndGitHubReleaseVersionsAreEqual(msixPackageVersion, gitHubReleaseTagName));
     }
 
     [Theory]
-    [InlineData("invalid-package-version", "1.2.3.4")]
+    [InlineData("invalid-package-version", "1.2.3.0")]
     public void CommonHelper_MsixPackageAndGitHubReleaseVersionsAreEqual_Exception(string msixPackageVersion, string gitHubReleaseTagName)
     {
         Assert.Throws<Exception>(() => CommonHelper.MsixPackageAndGitHubReleaseVersionsAreEqual(msixPackageVersion, gitHubReleaseTagName));
